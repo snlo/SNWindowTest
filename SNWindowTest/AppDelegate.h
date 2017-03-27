1cba205dd1175dcd024c1b2a5fcc0d5ca0fb8cb9
snlo <snloveydus@sina.com>
2017-03-27 15:31:11 +0800

    first commit

---
+++ SNWindowTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNWindowTest
+//
+//  Created by sunDong on 2017/3/27.
+//  Copyright © 2017年 snlo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNWindowTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNWindowTest
+//
+//  Created by sunDong on 2017/3/27.
+//  Copyright © 2017年 snlo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
